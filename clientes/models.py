464278fd19fb2b47f83cc8e4bd5c40f5a16ff185
Alan <alantinoco@gmail.com>
2022-01-08 19:44:35 -0300

    Add class Cliente em models.py

--- clientes/models.py
+++ clientes/models.py
@@ -1,1 +1,25 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
+
+class Cliente(models.Model):
+    
+    AGENDADO =  (
+        ("S", "Sim"),
+        ("N", "Não"),
+    )
+
+    
+    atendente = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=100, null=True, blank=True)
+    telefone = models.CharField(max_length=50)
+    email= models.EmailField(max_length=50, null=True, blank=True)
+    agendado = models.CharField(max_length=50, choices=AGENDADO)
+    data = DateTimeField(auto_now_add=True)
+    observações = models.TextField()
+    
+    def __str__(self):
+        return self.nome
