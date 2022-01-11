3fcbf9edb0a8e94aaca7fa72070bed9029db638f
alantinoco <alantinoco@gmail.com>
2022-01-10 22:34:59 -0300

    Adicionado campo cpf na classe cliente em models.py

--- clientes/models.py
+++ clientes/models.py
@@ -15,6 +15,7 @@ class Cliente(models.Model):
     atendente = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=100, null=True, blank=True)
+    cpf = models.CharField(max_length=11)
     telefone = models.CharField(max_length=50)
     email= models.EmailField(max_length=50, null=True, blank=True)
     agendado = models.CharField(max_length=50, choices=AGENDADO)
