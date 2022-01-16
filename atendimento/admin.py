0c95792005f729eed1e4c83a39da0e50716f809b
alantinoco <alantinoco@gmail.com>
2022-01-16 01:01:29 -0300

    Adicionado FormaPagamento model em admin.py

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Contato, Venda
+from .models import Contato, Venda, FormaPagamento
 
 admin.site.register(Contato)
 admin.site.register(Venda)
+admin.site.register(FormaPagamento)
